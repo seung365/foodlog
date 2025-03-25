94980529070b564ca8ae6cfc2f07af38643795a8
beom <bdh3659@naver.com>
2025-03-26 06:54:04 +0900

    feat: header에서 nav-link 컴포넌트 사용

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,29 +1,30 @@
 import Link from "next/link";
-import logoImag from '@/assets/logo.png';
-import classes from './main-header.module.css';
+import logoImag from "@/assets/logo.png";
+import classes from "./main-header.module.css";
 import Image from "next/image";
-import MainHeaderBackground from '@/components/main-header/main-header-background';
+import MainHeaderBackground from "@/components/main-header/main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
-    return (
-      <>
-        <MainHeaderBackground/>
-        <header className={classes.header}>
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
         <Link className={classes.logo} href="/">
-          <Image src={logoImag} alt="A plate with food" priority/>
+          <Image src={logoImag} alt="A plate with food" priority />
           NextLevel Foods
         </Link>
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
       </header>
     </>
-    )
+  );
 }
