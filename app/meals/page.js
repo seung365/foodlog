40a9ec8f882f8dc2ddd8da61197a65f19857b4b6
beom <bdh3659@naver.com>
2025-03-30 00:03:27 +0900

    feat: meals page loading suspense 처리

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,32 @@
-export default function Meals() {
-    return (
-        <div>
-            <h1>Meals</h1>
-            <p>Here is a list of meals</p>
-        </div>
-    )
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+import Link from "next/link";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created
+          <span className={classes.highlight}> by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook if for your loved ones.</p>
+        <p className={classes.cta}>
+          <Link href="meals/share">Share your recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fecthing meals....</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 }
